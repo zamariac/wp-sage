e20ce1fafc3ac0536a0e3d8a94f112bcc822e9af
maria <zamora.mariac@gmail.com>
2019-11-06 12:34:10 -0600

    react app added

---
+++ site/web/app/themes/barebones/index.php
@@ -0,0 +1,45 @@
+Html Webpack Plugin:
+<pre>
+  Error: Child compilation failed:
+  Module build failed: Error: ENOENT: no such file or directory, open '/Users/mariazamora/jnglsct-webdev/site/  web/app/themes/barebones/react-src/node_modules/lodash/lodash.js':
+  Error: ENOENT: no such file or directory, open '/Users/mariazamora/jnglsct-webdev/site/web/app/themes/barebo  nes/react-src/node_modules/lodash/lodash.js'
+  
+  - compiler.js:141 
+    [react-src]/[html-webpack-plugin]/lib/compiler.js:141:18
+  
+  - Compiler.js:343 
+    [react-src]/[webpack]/lib/Compiler.js:343:11
+  
+  - Compiler.js:681 
+    [react-src]/[webpack]/lib/Compiler.js:681:15
+  
+  
+  - Hook.js:154 AsyncSeriesHook.lazyCompileHook
+    [react-src]/[tapable]/lib/Hook.js:154:20
+  
+  - Compiler.js:678 
+    [react-src]/[webpack]/lib/Compiler.js:678:31
+  
+  
+  - Hook.js:154 AsyncSeriesHook.lazyCompileHook
+    [react-src]/[tapable]/lib/Hook.js:154:20
+  
+  - Compilation.js:1423 
+    [react-src]/[webpack]/lib/Compilation.js:1423:35
+  
+  
+  - Hook.js:154 AsyncSeriesHook.lazyCompileHook
+    [react-src]/[tapable]/lib/Hook.js:154:20
+  
+  - Compilation.js:1414 
+    [react-src]/[webpack]/lib/Compilation.js:1414:32
+  
+  
+  - Hook.js:154 AsyncSeriesHook.lazyCompileHook
+    [react-src]/[tapable]/lib/Hook.js:154:20
+  
+  - Compilation.js:1409 
+    [react-src]/[webpack]/lib/Compilation.js:1409:36
+  
+  
+</pre><script src="/app/themes/barebones/static/js/bundle.js?75bd1b3a185a0e6e8608"></script><script src="/app/themes/barebones/static/js/main.chunk.js?75bd1b3a185a0e6e8608"></script>
